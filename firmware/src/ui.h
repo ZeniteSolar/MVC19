032f3e6db8aea2511052bf87d1aef1d3d64a1664
João Antônio Cardoso <joao.maker@gmail.com>
2019-04-09 21:57:32 -0300

    initial UI idea

--- firmware/src/ui.h
+++ firmware/src/ui.h
@@ -5,6 +5,14 @@
  *
  * @brief This module implements a simple UI
  *
+ *  ________________
+ * | BAT.  | CURR.  |
+ * |________________|
+ * |M:36.1V|I:042.2A|
+ * |A:11.2V|O:100.2A|
+ * |S:13.0V|        |
+ * |________________|
+ *
  */
 
 #ifndef _UI_H_
@@ -15,5 +23,11 @@
 #include "usart.h"
 
 void ui_init(void);
+
+void update_battery_voltage_main(const char *s);
+void update_battery_voltage_auxiliary(const char *s);
+void update_battery_voltage_security(const char *s);
+void update_battery_current_input(const char *s);
+void update_battery_current_output(const char *s);
 
 #endif /* ifndef _UI_H_ */
