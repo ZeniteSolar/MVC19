7f772941fe2a9b5cfbc409bcf1e549bbba0efdb7
João Antônio Cardoso <joao.maker@gmail.com>
2019-04-09 21:40:37 -0300

    link and path fixed

--- firmware/src/display.h
+++ firmware/src/display.h
@@ -12,11 +12,11 @@
 
 #include <avr/io.h>
 //#include "../lib/bit_utils.h"
-//#include "conf.h"
-//#include "../lib/avr-ssd1306/fonts/Font_9_Mono.h"
-//#include "../lib/avr-ssd1306/fonts/Font_8_Myke.h"
-#include "../lib/avr-ssd1306/fonts/Font_3_PicoPixel.h"
-#include "../lib/avr-ssd1306/ssd1306.h"
+#include "conf.h"
+#include "../lib/DISPLAYS/SSD1306_AVR/ssd1306.h"
+//#include "../lib/DISPLAYS/SSD1306_AVR/fonts/Font_9_Mono.h"
+//#include "../lib/DISPLAYS/SSD1306_AVR/fonts/Font_8_Myke.h"
+#include "../lib/DISPLAYS/SSD1306_AVR/fonts/Font_3_Picopixel.h"
 
 void display_init();
 
