9ebdb64bf8104442d1dfcd01cbb77e5b3a77cd1c
guilhermecv <guilherme.valese@hotmail.com>
2019-08-23 15:10:17 -0300

    conversao dos valores para float

--- firmware/src/ui.c
+++ firmware/src/ui.c
@@ -30,27 +30,32 @@ void ui_draw_layout(void)
 
 void ui_update_battery_voltage_main(uint16_t num)
 {
-    display_send_uint16(num, col2, line2);
+    float val = num / 1000.f;
+    display_send_float(val, col2, line2);
 }
 
 void ui_update_battery_voltage_auxiliary(uint16_t num)
 {
-    display_send_uint16(num, col2, line3);
+    float val = num / 1000.f;
+    display_send_float(val, col2, line3);
 }
 
 void ui_update_battery_voltage_extra(uint16_t num)
 {
-    display_send_uint16(num, col2, line4);
+    float val = num / 1000.f;
+    display_send_float(val, col2, line4);
 }
 
 void ui_update_battery_current_input(uint16_t num)
 {
-    display_send_uint16(num, col4, line2);
+    float val = num / 100.f;
+    display_send_float(val, col4, line2);
 }
 
 void ui_update_battery_current_output(uint16_t num)
 {
-    display_send_uint16(num, col4, line3);
+    float val = num / 100.f;
+    display_send_float(val, col4, line3);
 }
 
 void ui_update_boat_rpm(uint16_t num)
