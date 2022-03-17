b8111a4c037fb51ea7f1184922af0df2a22f2386
guilherme.valese@hotmail.com <guilherme.valese@hotmail.com>
2022-03-17 00:20:50 -0300

    Tensões do banco exibidas individualmente

--- firmware/src/ui.c
+++ firmware/src/ui.c
@@ -24,44 +24,36 @@ void ui_init(void)
  */
 void ui_update_main_battery_voltage(void)
 {
-    static uint8_t i;
-
-    if(system_flags.no_message_from_MSC19_1 || system_flags.no_message_from_MSC19_2 || system_flags.no_message_from_MSC19_3)
-    {
+    if (system_flags.no_message_from_MSC19_1)
         display_send_string(DESCONNECTED_MESSAGE, COL1, LINE1, font_selected);
-    }
     else
-    {
-	if(undervoltage.main_bank)
-	{
-	    i++;
-	    if(i < 4)
-        	display_send_float((battery_voltage.main_bank/VSCALE_FLOAT), COL1, LINE1, font_selected);
-	    else if(i == 4)
-		display_send_string(("BAT. LOW"), COL1, LINE1, font_selected);
-	    else if(i == 8)
-		i = 0;
-	}
-	else
-            display_send_float((battery_voltage.main_bank/VSCALE_FLOAT), COL1, LINE1, font_selected);
-    }
-}
+        display_send_float((battery_voltage.main_cell_1 / VSCALE_FLOAT), COL1, LINE1, font_selected);
 
+    if (system_flags.no_message_from_MSC19_2)
+        display_send_float((battery_voltage.main_cell_2 / VSCALE_FLOAT), COL1, LINE2, font_selected);
+    else
+        display_send_string(DESCONNECTED_MESSAGE, COL1, LINE1, font_selected);
+
+    if (system_flags.no_message_from_MSC19_3)
+        display_send_float((battery_voltage.main_cell_3 / VSCALE_FLOAT), COL1, LINE3, font_selected);
+    else
+        display_send_string(DESCONNECTED_MESSAGE, COL1, LINE1, font_selected);
+}
 
 /**
  * @brief Atualiza a corrente medida nas baterias
  */
 void ui_update_battery_current(void)
 {
-    if(system_flags.no_message_from_MSC19_4)
+    if (system_flags.no_message_from_MSC19_4)
         display_send_string(DESCONNECTED_MESSAGE, COL1, LINE1, font_selected);
     else
-        display_send_float((battery_current.in/VSCALE_FLOAT), COL1, LINE1, font_selected);
+        display_send_float((battery_current.in / VSCALE_FLOAT), COL1, LINE1, font_selected);
 
-    if(system_flags.no_message_from_MSC19_5)
+    if (system_flags.no_message_from_MSC19_5)
         display_send_string(DESCONNECTED_MESSAGE, COL1, LINE2, font_selected);
     else
-        display_send_float((battery_current.out/VSCALE_FLOAT), COL1, LINE2, font_selected);
+        display_send_float((battery_current.out / VSCALE_FLOAT), COL1, LINE2, font_selected);
 }
 
 /**
@@ -69,7 +61,7 @@ void ui_update_battery_current(void)
  */
 void ui_update_rpm(void)
 {
-    if(system_flags.no_message_from_MT19)
+    if (system_flags.no_message_from_MT19)
         display_send_string(DESCONNECTED_MESSAGE, COL1, LINE3, font_selected);
     else
         display_send_uint16(boat_rpm, COL1, LINE3, font_selected);
@@ -77,82 +69,83 @@ void ui_update_rpm(void)
 
 void ui_update(void)
 {
-    switch(screen)
+    switch (screen)
     {
-        default:
-        case VOLTAGE:
-            ui_update_main_battery_voltage();
-            break;
+    default:
+    case VOLTAGE:
+        ui_update_main_battery_voltage();
+        break;
 
-        case CURRENT:
-            ui_update_battery_current();
-            ui_update_rpm();
-            break;
+    case CURRENT:
+        ui_update_battery_current();
+        ui_update_rpm();
+        break;
 
-        case CURRENT_SMALL:
-            ui_update_battery_current();
-            ui_update_rpm();
-            break;
+    case CURRENT_SMALL:
+        ui_update_battery_current();
+        ui_update_rpm();
+        break;
     }
 }
 
 void ui_check_modules_failure(void)
 {
-
 }
 
 void ui_draw_layout(void)
 {
-    switch(screen)
+    switch (screen)
     {
-        default:
-        case VOLTAGE:
-            display_send_string("-VOLTAGES-", COL0, LINE0, font_big);
-            display_send_string("M:", COL0, LINE1, font_selected);
-            display_send_string("A:", COL0, LINE2, font_selected);
-            display_send_string("E:", COL0, LINE3, font_selected);
-            break;
+    default:
+    case VOLTAGE:
+        display_send_string("-VOLTAGES-", COL0, LINE0, font_big);
+        display_send_string("1:", COL0, LINE1, font_selected);
+        display_send_string("2:", COL0, LINE2, font_selected);
+        display_send_string("3:", COL0, LINE3, font_selected);
+        break;
 
-        case CURRENT:
-            display_send_string("-CURRENTS-", COL0, LINE0, font_big);
-            display_send_string(">:", COL0, LINE1, font_selected);
-            display_send_string("<:", COL0, LINE2, font_selected);
-            display_send_string("T:", COL0, LINE3, font_selected);
-            break;
+    case CURRENT:
+        display_send_string("--POWER--", COL0, LINE0, font_big);
+        display_send_string(">:", COL0, LINE1, font_selected);
+        display_send_string("<:", COL0, LINE2, font_selected);
+        display_send_string("T:", COL0, LINE3, font_selected);
+        break;
 
-        case CURRENT_SMALL:
-            display_send_string("-CURRENTS-", COL0, LINE0, font_small);
-            display_send_string(">:", COL0, LINE1, font_small);
-            display_send_string("<:", COL0, LINE2, font_small);
-            display_send_string("T:", COL0, LINE3, font_small);
-	    display_send_string("A:", COL2, LINE1, font_small);
-	    display_send_string("E:", COL2, LINE2, font_small);
-            break;
+    case CURRENT_SMALL:
+        display_send_string("-CURRENTS-", COL0, LINE0, font_small);
+        display_send_string(">:", COL0, LINE1, font_small);
+        display_send_string("<:", COL0, LINE2, font_small);
+        display_send_string("T:", COL0, LINE3, font_small);
+        display_send_string("A:", COL2, LINE1, font_small);
+        display_send_string("E:", COL2, LINE2, font_small);
+        break;
     }
 }
 
 void ui_select_screen(screen_t screen_selected)
 {
-    #ifdef VERBOSE_ON_DISPLAY
+#ifdef VERBOSE_ON_DISPLAY
     usart_send_string("\nui_select_screen: ");
-    #endif
+#endif
 
-    if(screen_selected < LAST_SCREEN)
+    if (screen_selected < LAST_SCREEN)
     {
         screen = screen_selected;
         display_clear();
         ui_draw_layout();
 
-        if(screen == CURRENT_SMALL)
+        if (screen == CURRENT_SMALL)
             font_selected = font_small;
         else
             font_selected = font_big;
 
-    #ifdef VERBOSE_ON_DISPLAY
-        if(screen == VOLTAGE)               usart_send_string("Voltage selected");
-        else if(screen == CURRENT)          usart_send_string("Current selected");
-        else if(screen == CURRENT_SMALL)    usart_send_string("Current_small selected");
-    #endif
+#ifdef VERBOSE_ON_DISPLAY
+        if (screen == VOLTAGE)
+            usart_send_string("Voltage selected");
+        else if (screen == CURRENT)
+            usart_send_string("Current selected");
+        else if (screen == CURRENT_SMALL)
+            usart_send_string("Current_small selected");
+#endif
     }
-
 }
