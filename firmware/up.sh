88dadf6277196a0094c2d93f996199c8514384c0
ayresgit <ayres808@gmail.com>
2022-07-21 15:51:22 -0300

    Using ttyS8, comment previous ports

--- firmware/up.sh
+++ firmware/up.sh
@@ -5,12 +5,13 @@ rm -rf bin obj
 make all
 #fuser -k /dev/ttyACM0
 #avrdude -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
-avrdude -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
+#avrdude -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
+avrdude -c arduino -P /dev/ttyS8 -p m328p -D -U flash:w:bin/firmware.elf
 
 # testando com arduino nano
 # avrdude -c arduino -P /dev/ttyUSB0 -b 57600 -p m328p -D -U flash:w:bin/firmware.elf
 
 figlet "MVC19"
 
-stty -F /dev/ttyACM0 57600
-cat /dev/ttyACM0
+stty -F /dev/ttyS8 57600
+cat /dev/ttyS8
