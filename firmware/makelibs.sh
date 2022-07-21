d41a3e5bb938014326203c3fc5b2804dc2a6e613
alejopm03 <perdomomilar@gmail.com>
2022-07-21 17:29:20 -0300

    fix make libs

--- firmware/makelibs.sh
+++ firmware/makelibs.sh
@@ -1,8 +1,8 @@
-cd lib/avr-can-lib/src/
-make clean
-make lib
-cd ../../oled_display
-make clean
-make
-cd ../../../
+#make avr-can-lib
+make -C lib/avr-can-lib/src clean
+make -C lib/avr-can-lib/src lib
+
+#make oled-display
+make -C lib/oled-display clean
+make -C lib/oled-display
 wget https://raw.githubusercontent.com/ZeniteSolar/CAN_IDS/master/can_ids.h -O src/can_ids.h
