a568000168ad89381fc5e355cd65c3f36810fe64
ayresgit <ayres808@gmail.com>
2022-10-06 17:54:52 -0300

    stty command

--- firmware/up.sh
+++ firmware/up.sh
@@ -5,12 +5,12 @@ rm -rf bin obj
 make all
 #fuser -k /dev/ttyACM0
 #avrdude -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
-avrdude -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
+avrdude -c arduino -P /dev/ttyS13 -p m328p -D -U flash:w:bin/firmware.elf
 
 # testando com arduino nano
 # avrdude -c arduino -P /dev/ttyUSB0 -b 57600 -p m328p -D -U flash:w:bin/firmware.elf
 
-figlet "MVC19"
-
-stty -F /dev/ttyACM0 57600
-cat /dev/ttyACM0
+#figlet "MVC19"
+stty 57600 < /dev/ttyS13 && cat /dev/ttyS13 
+#stty -F /dev/ttyS13 57600
+#cat /dev/ttyS13
