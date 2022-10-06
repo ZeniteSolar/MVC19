5cde7d7d348699350a93208c15a0294cc9a3870c
ayresgit <ayres808@gmail.com>
2022-10-06 17:49:22 -0300

    Created if condition for can_filters depending on SIGNATURE_SELF

--- firmware/src/can_filters.h
+++ firmware/src/can_filters.h
@@ -65,20 +65,38 @@
  * If you want to receive both 11 and 29 bit identifiers, set your filters
  * and masks as follows:
  */
+
+
+#if CAN_SIGNATURE_SELF == CAN_SIGNATURE_MVC19_1
 const uint8_t can_filter[] PROGMEM =
 {
     // Group 0
-    MCP2515_FILTER(0), // Filter 0
-    MCP2515_FILTER(0),   // Filter 1
-
+    MCP2515_FILTER(CAN_MSG_MSC19_1_ADC_ID),     // Filter 1
+    MCP2515_FILTER(CAN_MSG_MSC19_1_STATE_ID),   // Filter 0
+    
     // Group 1
-    MCP2515_FILTER(0), // Filter 0
-    MCP2515_FILTER(0), // Filter 0
-    MCP2515_FILTER(0),  // Filter 1
-    MCP2515_FILTER(0),    // Filter 1
+    MCP2515_FILTER(CAN_MSG_MSC19_2_ADC_ID),     // Filter 3
+    MCP2515_FILTER(CAN_MSG_MSC19_2_STATE_ID),   // Filter 2
+    MCP2515_FILTER(CAN_MSG_MSC19_3_ADC_ID),     // Filter 5
+    MCP2515_FILTER(CAN_MSG_MSC19_3_STATE_ID),   // Filter 4
 
-    MCP2515_FILTER(0),             // Mask 0 (for group 0)
-    MCP2515_FILTER(0),             // Mask 1 (for group 1)
+    MCP2515_FILTER(0b11111111),             // Mask 0 (for group 0)
+    MCP2515_FILTER(0b11111111),             // Mask 1 (for group 1)
 };
+#elif CAN_SIGNATURE_SELF == CAN_SIGNATURE_MVC19_2
+const uint8_t can_filter[] PROGMEM =
+{
+    // Group 0
+    MCP2515_FILTER(CAN_MSG_MSC19_4_ADC_ID),     // Filter 1
+    MCP2515_FILTER(CAN_MSG_MSC19_4_STATE_ID),   // Filter 0
+    
+    // Group 1
+    MCP2515_FILTER(CAN_MSG_MSC19_5_ADC_ID),     // Filter 3
+    MCP2515_FILTER(CAN_MSG_MSC19_5_STATE_ID),   // Filter 2
+
+    MCP2515_FILTER(0b11111111),             // Mask 0 (for group 0)
+    MCP2515_FILTER(0b11111111),             // Mask 1 (for group 1)
+};
+#endif
 
 #endif /* ifndef CAN_FILTERS_H */
